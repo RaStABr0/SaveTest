71fa6fa4465c2f41786235807ede1f267108256b
Babkin Artyom <dr.v991149@gmail.com>
2020-10-28 02:46:39 +0300

    Init

---
+++ Assets/Scripts/Abstracts/SavableBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public abstract class SavableBase : MonoBehaviour
+{
+    [SerializeField] private string _id;
+    
+    public string Id { get; private set; }
+
+    public abstract void Save();
+    
+    public abstract void Load();
+    
+    public abstract void SetData();
+    
+    private void OnValidate()
+    {
+        if (Id == string.Empty)
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+    }
+
+    private abstract class SavableData
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Savable1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public class Savable1 : SavableBase
+{
+    private struct Savable1Data
+    {
+        public TransformData Transform;
+        
+    }
+
+    public override void Save()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Load()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void SetData()
+    {
+        throw new NotImplementedException();
+    }
+}
